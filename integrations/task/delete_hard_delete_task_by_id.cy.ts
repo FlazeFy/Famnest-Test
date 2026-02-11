6787ac9ad2576cffae94e75ed799efc29a59d899
FlazeFy2 <flazen.edu@gmail.com>
2026-02-11 08:31:43 +0700

    test(task): add integration tests for DELETE permanently task by id API (1 success, 3 failed cases)

---
+++ integrations/task/delete_hard_delete_task_by_id.cy.ts
@@ -0,0 +1,70 @@
+describe("Task API - DELETE Task By Id", () => {
+    const url = "/api/v1/tasks"
+    const id = Cypress.env("id")
+    let userToken: any 
+    let adminToken: any
+
+    before(() => {
+        cy.useLogin("user").then((token: string) => {
+            userToken = token
+        })
+        cy.useLogin("admin").then((token: string) => {
+            adminToken = token
+        })
+    })
+
+    describe("Failed cases", () => {
+        it("should fail if the task is deleted by admin", () => {
+            cy.request({
+                method: "DELETE",
+                url: `${url}/${id.task}`, 
+                headers: {
+                    Authorization: `Bearer ${adminToken}`
+                },
+                failOnStatusCode: false
+            }).then((res) => {
+                cy.expectDefaultResponseProps(res, 403, 'Your role is not authorized')
+            })
+        })
+        it("should fail if the task not found", () => {
+            cy.request({
+                method: "DELETE",
+                url: `${url}/${id.not_found}`, 
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                },
+                failOnStatusCode: false
+            }).then((res) => {
+                cy.expectDefaultResponseProps(res, 404, 'Task not found')
+            })
+        })
+        it("should fail with failed validation : id is not valid uuid", () => {
+            cy.request({
+                method: "DELETE",
+                url: `${url}/${id.invalid}`, 
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                },
+                failOnStatusCode: false
+            }).then((res) => {
+                cy.expectDefaultResponseProps(res, 422, 'Validation error')
+                cy.expectKeyExist(res.body.data, ["id"])
+                expect(res.body.data.id).to.eq("id must be at least 36 characters")
+            })
+        })
+    })
+
+    describe("Success cases", () => {
+        it("should delete task successfully with valid id", () => {
+            cy.request({
+                method: "DELETE", 
+                url: `${url}/${id.task}`, 
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                }
+            }).then((res) => {
+                cy.expectDefaultResponseProps(res, 200, 'Delete task successful')
+            })
+        })
+    })
+})
